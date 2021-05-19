a5df918e4d22048924c4de78260e979e9f7db2a5
Sai Shibu N B <saishibu38380@gmail.com>
2021-05-19 10:21:41 +0530

    Create example.py

---
+++ API/example.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+#Create a flask application
+app = Flask(__name__)
+
+#Define a function when <ip>:<port>/ is accessed on browser
+@app.route('/')
+def welcome():
+  #Return what you want to show on browser page
+  return "Welcome to Amrita Intelligent Infrastructure Data Management and Control Panel App. \n Use one of the options below."
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0",port=5000,debug=1)
