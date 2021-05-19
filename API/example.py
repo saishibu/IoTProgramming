36b4defb21e230ef7f4de7db4ee3969c181beda5
Sai Shibu <saishibu38380@gmail.com>
2021-05-19 12:32:19 +0530

    hello

--- API/example.py
+++ API/example.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 #Define a function when <ip>:<port>/ is accessed on browser
 @app.route('/')
+def hello():
+  return "hello world"	
+@app.route('/hello')
 def welcome():
   #Return what you want to show on browser page
   return "Welcome to Amrita Intelligent Infrastructure Data Management and Control Panel App. \n Use one of the options below."
