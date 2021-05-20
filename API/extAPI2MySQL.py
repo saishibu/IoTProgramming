9bae3711c4db3b3c09f2b79ba6d42a430bd68fc5
Sai Shibu N B <saishibu38380@gmail.com>
2021-05-20 16:39:40 +0530

    Update extAPI2MySQL.py

--- API/extAPI2MySQL.py
+++ API/extAPI2MySQL.py
@@ -1,11 +1,11 @@
-#NOTE: This program works for python2. Not tested for python3
-#You can test it on python3. Suppose if there is any error especially with urllib, try running with python2
+#NOTE: This program works for python2 & python3
+#Suppose if there is any error, especially with urllib (uncomment), try running with python2
 
 from flask import Flask
 import pymysql
 
 #Library to access external APIs (NOTE: This may not work with python3)
-from urllib.request import urlopen
+#from urllib.request import urlopen
 
 #For Python3 only
 import requests
@@ -16,23 +16,31 @@ import requests
 #Insert your OpenWeatherMap API here
 OPEN_WEATHER_MAP_API_KEY = "95c24917f8cc445dad4ea6113a933807"
 
+#give your Lattitude and Longitude
+lat = '8.01'
+lon = '77.05'
+
+#Create openweathermap url
+api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&exclude=hourly,daily,minutely,alerts+&appid="+OPEN_WEATHER_MAP_API_KEY+"&units=metric"
+ 
 app = Flask(__name__)
 @app.route('/fetchWeather')
 def get_open_weather_map_data():
-  #give your Lattitude and Longitude
-  lat = '8.01'
-  lon = '77.05'
   
-  #Create openweathermap url
-  api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&exclude=hourly,daily,minutely,alerts+&appid="+OPEN_WEATHER_MAP_API_KEY+"&units=metric"
-  print(api_url)
   #For Python2
   #response = urlopen(api_url).read()
   
   #For Python3
   response = requests.get(api_url).json()
+  
   print(response)
   return response
+
+@app.route('/todb')
+def todb():
+    response = requests.get(api_url).json()
+    print(response['current'])
+
 # conn = mysql.connect()
 # cur=conn.cursor()
 
