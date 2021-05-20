015bca1bd8f43f3b95545462e1d3e7e0dcd89c7d
Sai Shibu N B <saishibu38380@gmail.com>
2021-05-20 10:33:24 +0530

    Update MQTT_Subscribe.py

--- MQTT/MQTT_Subscribe.py
+++ MQTT/MQTT_Subscribe.py
@@ -1,7 +1,11 @@
 import paho.mqtt.client as mqtt
-
+	
+	broker="localhost"
+	port=1883
+	topic = "mqtt/gcp"
+	
 	client = mqtt.Client()
-	client.connect(broker,poer,0)
+	client.connect(broker,port,0)
 	client.subscribe(topic)
 	client.on_message = on_message
 	client.loop_forever()
