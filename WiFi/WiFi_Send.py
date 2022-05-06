143b16031c5e151131f663bfb02a118673fad9d1
Sai Shibu N B <saishibu38380@gmail.com>
2022-05-06 11:09:54 +0530

    Create WiFi_Send.py

---
+++ WiFi/WiFi_Send.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#Simple Python program to send data to another RPi over WiFi. Both devices must in same network and ports must be configured correctly.
+
+import socket
+
+TCP_IP = '<insert IP here>'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello from "+ TCP_IP
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
