7eb9ef087e73ac282c38a6ab563d5e7cef36fb84
Sai Shibu N B <saishibu38380@gmail.com>
2022-05-20 11:30:30 +0530

    Update WiFi_Send.py

--- WiFi/WiFi_Send.py
+++ WiFi/WiFi_Send.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+#http://www.steves-internet-guide.com/install-mosquitto-linux/
+
 #Simple Python program to send data to another RPi over WiFi. Both devices must in same network and ports must be configured correctly.
 
 import socket
